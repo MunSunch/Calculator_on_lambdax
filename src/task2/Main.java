d9aa1c021fe55fd699c1061f9bff9c93c1850654
MunirTheBest <msunchalyaev@gmail.com>
2023-01-30 14:36:44 +0300

    Finish_finish

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener anotherListener = System.out::println;
 
-        Worker worker = new Worker(listener);
+        Worker worker = new Worker(listener, anotherListener);
         worker.start();
     }
 }
